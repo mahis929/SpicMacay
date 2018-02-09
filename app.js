caa547a97d3b10e1482fdcb6421cb3d4bfbb3a8d
Mahesh <mahesh86701997@gmail.com>
2018-02-09 11:08:09 +0530

    3

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require("express"),
-    app     = express();
+    app     = express(),
+    bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
-    res.send("Hii");
+    res.render("landing");
 });
 
 app.listen(3000, function(){
