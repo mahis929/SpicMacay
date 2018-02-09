c0d6837e24531285b1ba9acf656b7323909beb94
Mahesh <mahesh86701997@gmail.com>
2018-02-09 11:32:57 +0530

    3

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res){
-    res.render("landing");
-});
+var indexRoutes = require("./routes/index"),
+    homeRoutes  = require("./routes/home");
+
+
+app.use("/", indexRoutes);
+app.use("/home", homeRoutes);
 
 app.listen(3000, function(){
     console.log("SpicMacay Server has started..!!!");
