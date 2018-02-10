91dfc5aad4636f51cf177521ebf42a0b11f80296
Mahesh <mahesh86701997@gmail.com>
2018-02-10 16:39:41 +0530

    hail

---
+++ public/scripts/home.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function(){
+    setBindings();
+});
+
+function setBindings(){
+    $('nav ul li a').click(function(e){
+        e.preventDefault();
+        var sectionId = e.currentTarget.id + 'Section';
+        alert(sectionId);
+
+        $('html body').animate({
+            scrollTop: $('#' + sectionId).offSet().top;
+        }, 1000);
+
+    });
+}*/
--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,9 +1,28 @@
+/*$('html').click(function(){
+    alert($(window).width())
+})*/
+
+if (window.matchMedia('(max-width: 1225px)').matches)
+{
+    $('nav ul li').removeClass('blk');
+} else {
+    $('nav ul li').addClass('blk');
+}
+
 $(window).on('scroll', function(){
     if($(window).scrollTop()){
         $('nav').addClass('black');
+        /*if (window.matchMedia('(max-width: 1220px)').matches)
+        {
+            $('nav ul li').removeClass('blk');
+        }*/
         $('nav ul li').removeClass('blk');
     } else {
         $('nav').removeClass('black');
+        /*if (window.matchMedia('(max-width: 1220px)').matches)
+        {
+            $('nav ul li').addClass('blk');
+        }*/
         $('nav ul li').addClass('blk');
     }
 });
