585bc49fd6b1c07a9f0114e2c93d82279bfd867a
Mahesh <mahesh86701997@gmail.com>
2018-02-10 20:23:19 +0530

    navlanding

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-var indexRoutes = require("./routes/index"),
-    homeRoutes  = require("./routes/home");
-
+var indexRoutes = require("./routes/index");
 
 app.use("/", indexRoutes);
-app.use("/home", homeRoutes);
 
 app.listen(3000, function(){
     console.log("SpicMacay Server has started..!!!");
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", function(req, res){
     res.render("landing");
 });
 
+router.get("/home", function(req, res){
+    res.render("home/index");
+});
+
+router.get("/about", function(req, res){
+    res.render("home/about");
+});
+
 module.exports = router;
