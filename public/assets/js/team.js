28a4056f02d9f144968ed9305360a5b3d93d3753
Mahesh Tudu <mahesh86701997@gmail.com>
2018-02-17 02:40:02 +0530

    Added team page

---
+++ public/assets/js/team.js
@@ -0,0 +1,7 @@
+function openSlideMenu(){
+      document.getElementById('side-menu').style.width = '300px';
+    }
+
+    function closeSlideMenu(){
+      document.getElementById('side-menu').style.width = '0';
+    }
